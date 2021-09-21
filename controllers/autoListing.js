78380844ac90777e54f2bf9d854c0a7020272f3d
criscama <cristian_camacho@aol.com>
2021-09-21 19:51:06 -0400

    Fixed html partials, added new route with ability to add auto.

--- controllers/autoListing.js
+++ controllers/autoListing.js
@@ -4,8 +4,22 @@ const router = express.Router()
 const Auto = require('../models/auto')
 
 router.get('/', (req, res) => {
-    // res.render('index.ejs')
-    res.send('test')
+    res.render('index.ejs')
+})
+
+router.get('/new', (req, res) => {
+    res.render('new.ejs')
+})
+
+router.post('/new', (req, res) => {
+    Auto.create(req.body, (error, newAuto) => {
+        if(error) {
+            console.log(error)
+        } else {
+            console.log(newAuto)
+        }
+        res.redirect('/al')
+    })
 })
 
 module.exports = router
