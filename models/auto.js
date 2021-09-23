ca63c4b4c509a16db0cacda3b247b555e787d9dc
criscama <cristian_camacho@aol.com>
2021-09-22 22:56:27 -0400

    Some final touches for the night.

--- models/auto.js
+++ models/auto.js
@@ -6,7 +6,7 @@ const autoSchema = new Schema({
     make: {type: String, required: true},
     model: {type: String, required: true},
     mileage: {type: Number, reqired: true},
-    img: { type: String },
+    img: { type: String, default: 'http://localhost:3000/public/images/car-310650_1280.png' },
     price: {type: Number},
     trim: {type: String},
     body: {type: String},
